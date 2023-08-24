b185767c6d552cfa6be2d0e38ad1f075fd116990
Nol Patterson <nol.patterson1683@gmail.com>
2023-08-24 11:28:02 -0500

    Draft 1 part1.cpp

--- C++ Versions/part1CPP.cpp
+++ C++ Versions/part1CPP.cpp
@@ -9,23 +9,43 @@
 #include <iomanip>
 using namespace std;
 
-//Main function
-int main(int M, int N){
-    int m = M;
-    int n = N;
+//Function Prototypes
+int euclidianGCD(int, int, int*, int*);
+
+//Driver function
+int main(int m, int n){
     //Coefficients for a and b respectively such that ax+by = gcd(a, b)
     int x, y;
-    //Reserve variable, in order to move values around
-    int r;
 
-    //Base case
-    if(m == 0){
-        x = 0;
-        y = 1;
-        return n;
-    }
-    
-    
+    //Calculating the gcd and passing x and y back through the functions
+    int gcd = euclidianGCD(m, n, &x, &y);
+    cout << "gcd(" << m << ", " << n << ") = mx + ny = ";
+    cout << "(" << m << ")(" << x << ") + (" << n << ")(" << y << ") = " << gcd << endl;
 
     return 0;
+}
+
+//Function Definitions
+/*
+    Function Name: euclidianGCD()
+    Purpose: Calculate the gcd between a, b and the coefficients c and d such that ac+bd = gcd(a, b)
+*/
+int euclidianGCD(int a, int b, int *c, int *d){
+    //Base case
+    if(a == 0){
+        *c = 0;
+        *d = 1;
+        return b;
+    }
+
+    //Variables to store the values from the recursive call.
+    int cR, dR;
+    //Recursive call
+    int gcd = euclidianGCD(a, b, &cR, &dR);
+
+    //Updating after recursive call
+    *c = dR - ((b/a)*cR);
+    *d = cR;
+
+    return gcd;
 }
