f3d14ea79e77988617d08bf4b8e8b51a5e21f103
Nol Patterson <nol.patterson1683@gmail.com>
2023-08-28 10:39:08 -0500

    New approach part3
    Instead of attempting to use linked list to store prime factors trying a vector

---
+++ C++ Versions/part3Vector.cpp
@@ -0,0 +1,36 @@
+/*
+    Name: Nol Patterson
+    Date: 8/24/2023
+    File Name: part3Vector.cpp
+    Purpose: Write a program to execute the "Middle School" Algorithm for computing gcd
+*/
+//Libraries
+#include <vector>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+//Function Prototypes
+void primeFactorization(int, vector<int> &);
+
+//Driver Function
+int main(int argc, char* argv[]){
+    int m = stoi(argv[1]);
+    int n = stoi(argv[2]);
+    vector<int> primesM;
+    vector<int> primesN;
+
+    
+
+    return 0;
+}
+
+//Function Definitions
+/*
+    Function Name: primeFactorization()
+    Purpose: Factor a given integer into its prime factors, and store the factors in a vector passed by reference
+*/
+void primeFactorization(int x, vector<int> &vectorX){
+    
+}
