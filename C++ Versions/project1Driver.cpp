0801a182811be11a7fc7509bcad135c76c54d84d
Nol Patterson <nol.patterson1683@gmail.com>
2023-08-29 12:25:27 -0500

    Update project1Driver.cpp

--- C++ Versions/project1Driver.cpp
+++ C++ Versions/project1Driver.cpp
@@ -1,23 +1,26 @@
 /*
     Name: Nol Patterson
-    Date: 8/24/2023
+    Date: 8/29/2023
     File Name: project1.cpp
     Purpose: Execute 3 different Algorithms to calculate the gcd of two given numbers taken from the command line
              Euclid Algorithm
              Consecutive Integer Checking Algorithm
              "Middle School Algorithm"
 */
-//Libaries
+//Libaries and Header Fiels
 #include <cmath>
 #include <iomanip>
 #include <iostream>
 #include <vector>
 using namespace std;
 
-//Function Prototypes
-
-
 //Driver Function
+int main(int argc, char* argv[]){
+    int m = stoi(argv[1]);
+    int n = stoi(argv[2]);
+    
 
+    return 0;
+}
 
 //Function Definitions
