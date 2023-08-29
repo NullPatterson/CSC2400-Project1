54800d25132302a62366e33ef4d5029cae0355e5
Nol Patterson <nol.patterson1683@gmail.com>
2023-08-29 12:19:53 -0500

    Innit Commit project1Driver.cpp
    Innitial Commit for a driver file to  combine all parts of project 1 into one program

---
+++ C++ Versions/project1Driver.cpp
@@ -0,0 +1,23 @@
+/*
+    Name: Nol Patterson
+    Date: 8/24/2023
+    File Name: project1.cpp
+    Purpose: Execute 3 different Algorithms to calculate the gcd of two given numbers taken from the command line
+             Euclid Algorithm
+             Consecutive Integer Checking Algorithm
+             "Middle School Algorithm"
+*/
+//Libaries
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//Function Prototypes
+
+
+//Driver Function
+
+
+//Function Definitions
